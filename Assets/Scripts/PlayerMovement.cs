e4f45981f12f0d9bd1689f483cb61d6622d7b6bb
Axel Almeren <axel.almeren@edu.futuregames.nu>
2023-03-20 13:09:18 +0100

    sadads

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rB;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.OverlapBox(groundCheck.position, 0.2f, groundLayer);
+    }
+}
