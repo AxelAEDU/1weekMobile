01107f6277af3087f19b26e372e968ffbe20c23c
Axel Almeren <axel.almeren@edu.futuregames.nu>
2023-03-21 16:50:03 +0100

    fucking upp some things

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,71 +5,135 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rB;
+    //GameComponents
+    private Rigidbody rB;
+    private UserPlayer _userPlayer;
+
+    //Jumping
+    private float jumpingPower = 100f;
     public LayerMask groundLayer;
     private Vector3 _boxSize;
-    private float _maxDistance = 2;
+    private float _maxDistance = 1;
 
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 8f;
+    //Movment
+    private Vector2 Movements;
+    private float speed = 10f;
     private bool isFacingRight = true;
 
-    // Start is called before the first frame update
+
+
+    //Animator
+    private Animator anim;
+    private bool _isRunning = false;
+
+    public void OnEnable()
+    {
+        _userPlayer = new UserPlayer();
+        _userPlayer.Player.Enable();
+    }
+
+    public void OnDisable()
+    {
+        _userPlayer.Player.Disable();
+    }
+
+    private void Awake()
+    {
+        Physics.gravity = new Vector3(0, -300f, 0);
+    }
     void Start()
     {
-        
+        //Getting the Animator
+        anim = gameObject.GetComponentInChildren<Animator>();
+        //Getting the Rigidbody
+        rB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rB.velocity = new Vector2(horizontal * speed, rB.velocity.y);
+        //calling on Animation
+        Animation();
+        //calling on FacingRightDirection
+        FacingRightDirection();
+    }
+    private void FixedUpdate()
+    {
+        Movment();
+        Jump();
 
-        if(!isFacingRight && horizontal > 0f)
+    }
+
+    void Animation()
+    {
+        //Getting infomation from Animatorn to play running animation
+        _isRunning = (Movements.x > 0.1f || Movements.x < -0.1f) ? true : false;
+        anim.SetBool("isRunning", _isRunning);
+
+    }
+
+    void FacingRightDirection()
+    {
+        //Getting infomation on what direction player are moving
+        rB.velocity = new Vector2(Movements.x * speed, rB.velocity.y);
+
+        if (!isFacingRight && Movements.x > 0f)
         {
             Flip();
         }
-        else if (isFacingRight && horizontal < 0f)
+        else if (isFacingRight && Movements.x < 0f)
         {
             Flip();
         }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
+        //Will check if you are Grounded
         if (Physics.BoxCast(transform.position, _boxSize, -transform.up, transform.rotation, _maxDistance, groundLayer))
         {
+            Debug.Log("Grounded");
             return true;
         }
         else
         {
+            Debug.Log("NotGrounded");
             return false;
         }
     }
 
     private void Flip()
     {
+        //Rotate the charater in the direction you move
         isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
         localScale.x *= -1f;
         transform.localScale = localScale;
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Movment()
     {
-        horizontal = context.ReadValue<Vector2>().x;
+        Movements = _userPlayer.Player.Move.ReadValue<Vector2>();
+        rB.velocity = speed * Movements.y * transform.forward + (transform.right * Movements.x) * speed;
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    public void Jump()
     {
-        if(context.performed && IsGrounded())
+        if (_userPlayer.Player.Jump.triggered && IsGrounded())
         {
+            Debug.Log("jumping");
             rB.velocity = new Vector2(rB.velocity.x, jumpingPower);
         }
-        if(context.canceled && rB.velocity.y > 0f)
-        {
-            rB.velocity = new Vector2(rB.velocity.x, rB.velocity.y * 0.5f);
-        }
+
+
+
+
+    }
+
+
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("fire0");
     }
 }
